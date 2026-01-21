bc18ed4e2922d89e9ba9cbca71212dd75197a0fe
Magical <87448756+MagicalCipher@users.noreply.github.com>
2026-01-20 22:50:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,9 @@ const gamesContainer = document.getElementById("games-container");
 function addGamesToPage(games) {
 
     // loop over each item in the data
-
+        for (i = 0; i < games.length; i++) {
+            const game = games[i];
+        }
 
         // create a new div element, which will become the game card
 
